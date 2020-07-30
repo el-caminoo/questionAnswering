7d11866bb1e9cd0eb5359a23cd563830253a5345
Qalac <officialqalac@gmail.com>
2020-07-30 18:37:29 +0100

    change port nuber to allow https

--- api.py
+++ api.py
@@ -10,8 +10,8 @@ model = QA("model")
 
 @app.route("/",methods=['POST'])
 def predict():
-    doc = request.form.get("document")
-    q = request.form.get("question")
+    doc = request.json["document"]
+    q = request.json["question"]
     try:
         out = model.predict(doc,q)
         return jsonify({"result":out['answer'], "confidence-level": out['confidence']})
@@ -20,5 +20,5 @@ def predict():
         return jsonify({"result":"Model Failed"})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True, threaded=True)
+    app.run(host="127.0.0.1", port=443, debug=True, threaded=True)
     
