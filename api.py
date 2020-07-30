ce4c3cc418d570dbf34b4b24a74ad6b9a829c086
Qalac <officialqalac@gmail.com>
2020-07-30 02:39:25 +0100

    changed to test

--- api.py
+++ api.py
@@ -6,18 +6,9 @@ from bert import QA
 app = Flask(__name__)
 CORS(app)
 
-model = QA("model")
-
-@app.route("/",methods=['POST'])
+@app.route("/",methods=['GET'])
 def predict():
-    doc = request.form.get("document")
-    q = request.form.get("question")
-    try:
-        out = model.predict(doc,q)
-        return jsonify({"result":out['answer']})
-    except Exception as e:
-        print(e)
-        return jsonify({"result":"Model Failed"})
+    return 'hello world'
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(host="127.0.0.1", port=8000, debug=True, threaded = True)
