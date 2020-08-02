8f59ff398a0ff4303096c9c6accb57d0082dc0ad
Qalac <officialqalac@gmail.com>
2020-08-02 23:33:09 +0100

    d

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def predict():
     #     return jsonify({"result":"Model Failed"})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True, threaded=True)
+    app.run()
     
