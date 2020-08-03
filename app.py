06cd79b60a42dc38098f335878c7fb57fcf1c268
Qalac <officialqalac@gmail.com>
2020-08-03 12:19:03 +0100

    im trying

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
+from bert import QA
 
 app = Flask(__name__)
 CORS(app)
@@ -19,5 +20,5 @@ def predict():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-    
+
     
